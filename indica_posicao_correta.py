ab873cb0f8bc66c713b04cf3bfd065810ddc03e6
giuliaspiovesan <giuliaspiovesan@outlook.com>
2023-10-30 18:04:40 -0300

    codigo com erro

--- indica_posicao_correta.py
+++ indica_posicao_correta.py
@@ -1,2 +1,16 @@
 def inidica_posicao(sorteada, chute):
-    
+    saida = []
+    if len(sorteada) != len(chute):
+        return []
+    else:
+        for i in range(len(chute)):
+            letra_c = chute[i]
+            letra_s = sorteada[i]
+            if letra_c in sorteada:
+                if letra_c == letra_s:
+                    saida.append(0)
+                else:
+                    saida.append(1)
+            else:
+                saida.append(2)
+    return saida
